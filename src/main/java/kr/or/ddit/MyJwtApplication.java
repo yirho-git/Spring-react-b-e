424dc5289bd51c5769091ab9af9a285341c73bac
yirho <yirho0528@gmail.com>
2026-04-09 21:16:07 +0900

    init

---
+++ src/main/java/kr/or/ddit/MyJwtApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJwtApplication.class, args);
+    }
+
+}
